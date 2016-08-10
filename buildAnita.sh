478f45da8b5bf1ebf0f78c41d0d3acca672ee8b1
Ben Strutt <b.strutt.12@ucl.ac.uk>
2016-08-10 18:12:51 +0100

    Added self-updating to anitaBuildTool. Added githook to prevent commiting files in components subdirectory to anitaBuildTool repositories.

--- buildAnita.sh
+++ buildAnita.sh
@@ -2,13 +2,24 @@
 
 if [ -z "$ANITA_UTIL_INSTALL_DIR" ]; then
     echo "Need to set ANITA_UTIL_INSTALL_DIR"
-    echo "This is where the ANITA sofwtare will be installed"
+    echo "This is where the ANITA software will be installed"
     exit 1
 fi  
 
 hash root-config 2>/dev/null || { echo >&2 "I require root-config but it's not installed.  Aborting."; exit 1; }
 
 hash cmake 2>/dev/null || { echo >&2 "I require cmake but it's not installed.  Aborting."; exit 1; }
+
+
+#Step 0: Update myself, since I occasionally get updated.
+./checkForAnitaBuildToolUpdate.sh
+updated=$?
+if [ ${updated} -ne 0 ];then
+    echo "Please run ./buildAnita.sh again now the update is complete."
+    exit 1;
+fi
+ln -sf anitaGitHooks/pre-commit.sh .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
 
 
 #Step 1: To create the components directory and try and download the repositories
---
+++ checkForAnitaBuildToolUpdate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git fetch
+LOCAL=$(git rev-parse @{0})
+REMOTE=$(git rev-parse @{u})
+BASE=$(git merge-base @{0} @{u})
+if [ $LOCAL = $REMOTE ]; then
+    echo "anitaBuildTool is up to date."
+elif [ $LOCAL = $BASE ]; then
+    "Local anitaBuildTool is behind upstream. Attempting merge."
+    git merge
+    exit 1;
+elif [ $REMOTE = $BASE ]; then
+    echo "Local anitaBuildTool is ahead of upstream. Consider pushing your changes."
+else
+    echo "Local anitaBuildTool has diverged from upstream. Aborting update."
+fi
+echo "Continuing build."
+exit 0
