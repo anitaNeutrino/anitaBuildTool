b11125722cc54901afffe905b51a3816783aaaa7
LukeBatten <luke.batten.15@ucl.ac.uk>
2017-03-23 19:27:07 +0000

    Easier option for just using TreeMaker w/out aware stuf

--- updateComponents.sh
+++ updateComponents.sh
@@ -6,7 +6,10 @@ if [ ! -z $1 ]
 then  
   BUILDAWARE=$1 
 fi 
-
+if [ ! -z $2 ] 
+then  
+  BUILDAWARE=$2 
+fi 
 
 EVENT_READER_BRANCH=`cat which_event_reader` 
 
@@ -29,6 +32,33 @@ AN=$GH/anitaNeutrino
 if [[ $BUILDAWARE == 1 ]]; then
     echo "Will download the AWARE libraries as well"
     for repName in $RN/aware.git $AN/anitaTreeMaker.git $AN/anitaTelem.git $AN/anitaAwareFileMaker.git; do
+    thisDirWithGit=${repName##*/}
+    thisDir=${thisDirWithGit%.*}
+    if [ -d "$thisDir" ]; then
+	echo "$thisDir already exists, will update"
+	cd $thisDir
+        if [[ $repName == *"eventReaderRoot"* ]] ; then
+          git checkout $EVENT_READER_BRANCH 
+        fi
+
+        git pull
+
+	cd ..
+    else
+	echo "Fetching $thisDir"
+        if [[ $repName == *"eventReaderRoot"* ]] ; then
+          git clone -b $EVENT_READER_BRANCH $repName
+        else
+          git clone $repName
+        fi
+    fi
+done	
+    
+fi
+
+if [[ $BUILDAWARE == 2 ]]; then
+    echo "Will download the anitaTreeMaker as well"
+    for repName in $AN/anitaTreeMaker.git; do
     thisDirWithGit=${repName##*/}
     thisDir=${thisDirWithGit%.*}
     if [ -d "$thisDir" ]; then
