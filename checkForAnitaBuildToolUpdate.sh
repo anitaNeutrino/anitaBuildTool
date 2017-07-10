cb2475acc2f5fc1c8304b727fee0ab357a929e86
Ben Strutt <strutt@physics.ucla.edu>
2017-07-10 15:21:20 -0700

    Check anitaBuildTool was cloned rather than downloaded

--- checkForAnitaBuildToolUpdate.sh
+++ checkForAnitaBuildToolUpdate.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
+# First check if we have a .git subdirectory
+if [ ! -d ".git" ]; then
+    echo "Error! anitaBuildTool must be cloned from the anitaBuildTool repository. Aborting build."
+    echo "If you did not get the anitaBuildTool by running this command:"
+    echo ""
+    echo "    git clone https://github.com/anitaNeutrino/anitaBuildTool"
+    echo ""
+    echo "then please redownload this repository using the above command"
+    echo "(this will create a directory called anitaBuildTool initialised with git)" 
+    exit 1;
+fi;
+
+
+# Update the repository
 git fetch
+fetchSuccess=$?
+
+if [ ${fetchSuccess} -gt 0 ]; then
+    echo "Error! anitaBuildTool could not connect to remote repository. Aborting build."
+    exit 2;
+fi;
+
+
 LOCAL=$(git rev-parse @{0})
 REMOTE=$(git rev-parse @{u})
 BASE=$(git merge-base @{0} @{u})
@@ -14,6 +36,8 @@ elif [ $REMOTE = $BASE ]; then
     echo "Local anitaBuildTool is ahead of upstream. Consider pushing your changes."
 else
     echo "Local anitaBuildTool has diverged from upstream. Aborting update."
-fi
+    exit 3;
+fi;
+
 echo "Continuing build."
-exit 0
+exit 0;
